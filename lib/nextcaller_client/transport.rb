33c8e2b880e221a9c60bc17f1ce308bfda509313
Dmitry Vlasov <d.vlasov.work@gmail.com>
2014-09-30 08:56:21 +0000

    fixed  1.9.2 'hostname' issue

--- lib/nextcaller_client/transport.rb
+++ lib/nextcaller_client/transport.rb
@@ -26,7 +26,8 @@ module NextcallerClient
       request['Connection'] = 'Keep-Alive'
       request['User-Agent'] = @user_agent if @user_agent
 
-      https = Net::HTTP.new(uri.hostname, uri.port)
+      hostname =  /\A\[(.*)\]\z/ =~ uri.host ? $1 : uri.host #  ruby prior to 1.9.3 does not have 'hostname', which removes brackets from ipv6 addresses
+      https = Net::HTTP.new(hostname, uri.port)
       https.read_timeout = DEFAULT_REQUEST_TIMEOUT
       https.use_ssl = true
 
